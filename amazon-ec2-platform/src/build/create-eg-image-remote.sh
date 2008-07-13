28430f3b8fe8645e8921a91d12f8ede1a7512695
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-07-13 14:17:04 +0000

    Fixed Copyright notices.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@5 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- amazon-ec2-platform/src/build/create-eg-image-remote.sh
+++ amazon-ec2-platform/src/build/create-eg-image-remote.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
+# Copyright (C) 2007-2008 Elastic Grid, LLC.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# Licensed under the GNU Lesser General Public License, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.gnu.org/licenses/lgpl-3.0.html
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
