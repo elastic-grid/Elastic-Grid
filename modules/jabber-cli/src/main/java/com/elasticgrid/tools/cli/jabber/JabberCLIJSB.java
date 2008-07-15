60910e1753220d8f762972dc7f94ae51c4ebbf06
Dennis Reedy <dennis.reedy@elastic-grid.com>
2008-07-15 14:53:16 +0000

    Changed to accomodate Rio re-packaging
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@17 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- modules/jabber-cli/src/main/java/com/elasticgrid/tools/cli/jabber/JabberCLIJSB.java
+++ modules/jabber-cli/src/main/java/com/elasticgrid/tools/cli/jabber/JabberCLIJSB.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2007-2008 Elastic Grid, LLC.
- * 
+ *
  * Licensed under the GNU Lesser General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.gnu.org/licenses/lgpl-3.0.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,11 +17,7 @@
 package com.elasticgrid.tools.cli.jabber;
 
 import org.jivesoftware.smack.*;
-import org.jivesoftware.smack.filter.AndFilter;
-import org.jivesoftware.smack.filter.FromContainsFilter;
-import org.jivesoftware.smack.filter.MessageTypeFilter;
-import org.jivesoftware.smack.filter.NotFilter;
-import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.*;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.packet.PacketExtension;
@@ -33,21 +29,23 @@ import org.rioproject.event.DynamicEventConsumer;
 import org.rioproject.event.RemoteServiceEvent;
 import org.rioproject.event.RemoteServiceEventListener;
 import org.rioproject.jsb.ServiceBeanAdapter;
-import org.rioproject.qos.SLAThresholdEvent;
 import org.rioproject.resources.util.RioVersion;
+import org.rioproject.sla.SLAThresholdEvent;
 import org.rioproject.tools.cli.CLI;
 import org.rioproject.tools.cli.OptionHandler;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.rmi.RemoteException;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.StringTokenizer;
 
 /**
  * @author Jerome Bernard
  */
-public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, RemoteServiceEventListener {
+public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI,
+                                                                RemoteServiceEventListener {
     protected MultiUserChat chat;
     protected XMPPConnection jabberConnection;
     private String jabberUser;
@@ -61,29 +59,30 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
     public void initialize(ServiceBeanContext context) throws Exception {
         super.initialize(context);
         jabberUser = (String) context.getConfiguration().getEntry(
-                "com.elasticgrid.tools.cli.jabber",
-                "jabberUser", String.class
+            "com.elasticgrid.tools.cli.jabber",
+            "jabberUser", String.class
         );
         jabberDomain = (String) context.getConfiguration().getEntry(
-                "com.elasticgrid.tools.cli.jabber",
-                "jabberDomain", String.class
+            "com.elasticgrid.tools.cli.jabber",
+            "jabberDomain", String.class
         );
         jabberPassword = (String) context.getConfiguration().getEntry(
-                "com.elasticgrid.tools.cli.jabber",
-                "jabberPassword", String.class
+            "com.elasticgrid.tools.cli.jabber",
+            "jabberPassword", String.class
         );
         jabberChatroom = (String) context.getConfiguration().getEntry(
-                "com.elasticgrid.tools.cli.jabber",
-                "jabberChatroom", String.class
+            "com.elasticgrid.tools.cli.jabber",
+            "jabberChatroom", String.class
         );
         try {
             logger.info("In init()");
             cli = CLI.getInstance();
             //CLI.initCLI(new String[] {});
             initializeJabberConnection();
-            consumer = new DynamicEventConsumer(SLAThresholdEvent.getEventDescriptor(),
-                                            this,
-                                            context.getDiscoveryManagement());
+            consumer =
+                new DynamicEventConsumer(SLAThresholdEvent.getEventDescriptor(),
+                                         this,
+                                         context.getDiscoveryManagement());
         } catch (Throwable t) {
             logger.log(Level.WARNING, "Can't connect to Jabber server", t);
             t.printStackTrace();
@@ -93,7 +92,8 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
     private void initializeJabberConnection() throws XMPPException {
         XMPPConnection.DEBUG_ENABLED = true;
         logger.info("Initialization of Jabber connection");
-        ConnectionConfiguration connectionConfiguration = new ConnectionConfiguration(jabberDomain, 5223, jabberDomain);
+        ConnectionConfiguration connectionConfiguration =
+            new ConnectionConfiguration(jabberDomain, 5223, jabberDomain);
         connectionConfiguration.setSecurityMode(ConnectionConfiguration.SecurityMode.enabled);
         logger.info("Initiating Jabber connection");
         jabberConnection = new XMPPConnection(connectionConfiguration);
@@ -112,20 +112,22 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
         } catch (Exception e) {
             chat.join(jabberUser);
         }
-        chat.invite("jerome.bernard@kalixia.com", "Rio platform administration");
-        chat.sendMessage("You are administering a Rio " + RioVersion.VERSION + " platform. With great power comes great responsability!");
+        chat.invite("jerome.bernard@kalixia.com",
+                    "Rio platform administration");
+        chat.sendMessage("You are administering a Rio " + RioVersion.VERSION +
+                         " platform. With great power comes great responsability!");
 
         // register for all incoming commands -- we're listening to all messages except those from us :-)
         PacketFilter filter = new AndFilter(
-                new MessageTypeFilter(Message.Type.chat),
-                new NotFilter(new FromContainsFilter(jabberUser))
+            new MessageTypeFilter(Message.Type.chat),
+            new NotFilter(new FromContainsFilter(jabberUser))
         );
         PacketListener cliListener = new CLIPacketListener();
         jabberConnection.addPacketListener(cliListener, filter);
     }
 
     public void destroy() {
-        if(consumer != null) {
+        if (consumer != null) {
             consumer.deregister(this);
             consumer.terminate();
         }
@@ -155,7 +157,9 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
                 } else {
                     OptionHandler handler = cli.getOptionHandler(option);
                     if (handler != null) {
-                        String response = handler.process(command, null, output);
+                        String response = handler.process(command,
+                                                          null,
+                                                          output);
                         if (response.length() > 0)
                             output.println(response);
                     }
@@ -173,23 +177,27 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
 
     /**
      * Keeps track of SLA events.
+     *
      * @param event the SLA event
      * @inheritDoc
      */
     public void notify(RemoteServiceEvent event) {
         if (event instanceof SLAThresholdEvent) {
-            SLAThresholdEvent slaEvent = (SLAThresholdEvent)event;
-            String message = slaEvent.getServiceElement().getName()+"."+
-                   slaEvent.getServiceElement().getOperationalStringName()+" "+
-                   "SLA ["+slaEvent.getSLA().getIdentifier()+"] "+
-                   (slaEvent.getType() == SLAThresholdEvent.BREACHED?"BREACHED":
-                    "CLEARED")+" " +
-                    "low="+slaEvent.getSLA().getCurrentLowThreshold()+", " +
-                    "high="+slaEvent.getSLA().getCurrentHighThreshold();
+            SLAThresholdEvent slaEvent = (SLAThresholdEvent) event;
+            String message = slaEvent.getServiceElement().getName() + "." +
+                             slaEvent.getServiceElement()
+                                 .getOperationalStringName()+
+                                 " SLA ["+slaEvent.getSLA().getIdentifier()+"] "+
+                                 (slaEvent.getType()==SLAThresholdEvent.BREACHED
+                                  ? "BREACHED":"CLEARED")+" " +
+                                 "low="+slaEvent.getSLA().getCurrentLowThreshold()+", "+
+                                 "high="+slaEvent.getSLA().getCurrentHighThreshold();
             logger.finest("Going to send message: " + message);
             try {
-                if (jabberConnection == null || !jabberConnection.isConnected()) {
-                    logger.log(Level.WARNING, "Not connected with the Jabber server. Trying to create one.");
+                if (jabberConnection == null ||
+                    !jabberConnection.isConnected()) {
+                    logger.log(Level.WARNING,
+                               "Not connected with the Jabber server. Trying to create one.");
                     initializeJabberConnection();
                 }
                 chat.sendMessage(message);
@@ -205,19 +213,24 @@ public class JabberCLIJSB extends ServiceBeanAdapter implements JabberCLI, Remot
         public void processPacket(Packet packet) {
             if (packet instanceof Message) {
                 Message message = (Message) packet;
-                PacketExtension extension = message.getExtension("x", "jabber:x:delay");
+                PacketExtension extension = message.getExtension("x",
+                                                                 "jabber:x:delay");
                 // skip the message is it is resent from an earlier conversation
-                if (extension != null && extension instanceof DelayInformation) {
+                if (extension != null &&
+                    extension instanceof DelayInformation) {
                     logger.info("Skipping command '" + message.getBody() + "'");
                     return;
                 }
                 try {
                     executeCommand(message.getBody());
                 } catch (RemoteException e) {
-                    throw new UnsupportedOperationException("Can't process command " + message.getBody(), e);
+                    throw new UnsupportedOperationException(
+                        "Can't process command " + message.getBody(),
+                        e);
                 }
             } else {
-                logger.log(Level.WARNING, "Can't process XMPP packet " + packet);
+                logger.log(Level.WARNING,
+                           "Can't process XMPP packet " + packet);
             }
         }
     }
