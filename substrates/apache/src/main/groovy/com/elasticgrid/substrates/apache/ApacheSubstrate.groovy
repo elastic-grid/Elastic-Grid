dc1cc6a73902d6ccecf3bab3a85469c1fc129067
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-07-23 22:02:19 +0000

    Initial work on an Apache 2 substrate.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@38 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

---
+++ substrates/apache/src/main/groovy/com/elasticgrid/substrates/apache/ApacheSubstrate.groovy
@@ -0,0 +1,47 @@
+package com.elasticgrid.substrates.tomcat6
+
+import com.elasticgrid.substrates.AbstractSubstrate
+import groovy.xml.MarkupBuilder
+import com.elasticgrid.substrates.FirewallRule
+import com.elasticgrid.substrates.FirewallRule.IpProtocol
+
+class ApacheSubstrate extends AbstractSubstrate {
+    def version
+
+    public String getName() {
+        return "Apache 2";
+    }
+
+    public void addDomainSpecificLangueFeatures(MarkupBuilder builder, ExpandoMetaClass emc) {
+        emc.tomcat = { Map attributes, Closure cl ->
+            version = attributes.version ?: '2.2.9'
+            def removeOnDestroy = attributes.removeOnDestroy ?: true
+            serviceExec(name: 'Apache') {
+                software(name: 'Apache', version: version, removeOnDestroy: removeOnDestroy) {
+                    install source: "https://elastic-grid.s3.amazonaws.com/apache-httpd/apache-httpd-${version}.zip",
+                            target: '${RIO_HOME}/system/external/apache-httpd', unarchive: true
+                    postInstall(removeOnCompletion: removeOnDestroy) {
+                        execute command: "/bin/chmod +x \${RIO_HOME}/system/external/apache-httpd/httpd-$version/bin/*.sh",
+                                nohup: false
+                    }
+                }
+                execute inDirectory: 'bin', command: 'apachectl start'
+                cl()
+                maintain 1
+            }
+        }
+        emc.site = { Map attributes ->
+            data source: attributes.source,
+                 target: "\${RIO_HOME}/system/external/apache-httpd/httpd-$version/htdocs/"
+        }
+    }
+
+    /**
+     * Open port 80 for TCP from anywhere.
+     */
+    @Override
+    public List<FirewallRule> getFirewallRules() {
+        [new FirewallRule(getName(), IpProtocol.TCP, 80, '??')]
+    }
+
+}
