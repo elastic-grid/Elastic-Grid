fac49cd6f6d0d8ab1f36f0cde1fc031366af21d4
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-10-12 19:28:04 +0000

    Added tests for the REST API.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@134 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

---
+++ modules/elastic-grid-manager/src/test/java/com/elasticgrid/cluster/ClusterManagerMock.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2007-2008 Elastic Grid, LLC.
+ * 
+ * This file is part of Elastic Grid.
+ * 
+ * Elastic Grid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * Elastic Grid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Elastic Grid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.elasticgrid.cluster;
+
+import com.elasticgrid.model.Cluster;
+import com.elasticgrid.model.ClusterException;
+import com.elasticgrid.model.ClusterNotFoundException;
+import com.elasticgrid.model.ec2.EC2Node;
+import com.elasticgrid.model.ec2.impl.EC2ClusterImpl;
+import com.elasticgrid.model.ec2.impl.EC2NodeImpl;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Mock for {@link ClusterManager}.
+ */
+public class ClusterManagerMock implements ClusterManager {
+    private Map<String, Cluster> clusters = new HashMap<String, Cluster>();
+
+    public void startCluster(String clusterName) throws ClusterException, RemoteException, ExecutionException, TimeoutException, InterruptedException {
+        startCluster(clusterName, 1);
+    }
+
+    public void startCluster(String clusterName, int size) throws ClusterException, ExecutionException, TimeoutException, InterruptedException, RemoteException {
+        startCluster(clusterName, size, 0);
+    }
+
+    public void startCluster(final String clusterName, int numberOfMonitors, int numberOfAgents) throws ClusterException, ExecutionException, TimeoutException, InterruptedException, RemoteException {
+        Cluster<EC2Node> cluster = new EC2ClusterImpl();
+        cluster.name(clusterName);
+        for (int i = 0; i < numberOfMonitors; i++) {
+            EC2NodeImpl node = new EC2NodeImpl();
+            node.instanceID(clusterName + '-' + i);
+            try {
+                node.address(InetAddress.getLocalHost());
+            } catch (UnknownHostException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            cluster.getNodes().add(node);
+        }
+        clusters.put(clusterName, cluster);
+    }
+
+    public void stopCluster(String clusterName) throws ClusterException, RemoteException {
+        // do nothing
+    }
+
+    public List<Cluster> findClusters() throws ClusterException, RemoteException {
+        return new ArrayList<Cluster>(clusters.values());
+    }
+
+    public Cluster cluster(String name) throws ClusterException, RemoteException {
+        return clusters.get(name);
+    }
+
+    public void resizeCluster(String clusterName, int newSize) throws ClusterNotFoundException, ClusterException, ExecutionException, TimeoutException, InterruptedException, RemoteException {
+        // do nothing
+    }
+}
