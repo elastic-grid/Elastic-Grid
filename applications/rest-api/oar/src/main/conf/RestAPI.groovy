1a99759b512eef3612cbae4e365bf57a03d5c25a
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-11-18 22:51:23 +0000

    Improvements to the REST OpString and private LAN platform.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@154 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

---
+++ applications/rest-api/oar/src/main/conf/RestAPI.groovy
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2007-2008 Elastic Grid, LLC.
+ *
+ * This file is part of Elastic Grid.
+ *
+ * Elastic Grid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * Elastic Grid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Elastic Grid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.util.logging.Level
+
+deployment(name:'REST Container') {
+    groups 'rio'
+
+    logging {
+        logger('com.elasticgrid.rest.RestJSB', Level.FINE)
+        logger('org.springframework', Level.FINE)
+    }
+
+    systemRequirements(id: 'Elastic Grid Platform') {
+        software name: 'Elastic Grid Kernel', version: '1.0'
+        software name: 'Elastic Grid Framework', version: '1.0'
+    }
+
+    service(name: 'REST Service') {
+        interfaces {
+            classes 'org.rioproject.resources.servicecore.Service'
+            resources 'rio-dl.jar'
+        }
+        implementation(class: 'com.elasticgrid.rest.RestJSB') {
+            resources 'rest-api-oar/lib/rest-api-service-0.8.2-impl.jar',
+                      'rest-api-oar/lib/org.restlet-1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/org.restlet.ext.spring-1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/org.restlet.ext.jibx-1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/org.restlet.ext.wadl-1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/com.noelios.restlet-1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/com.noelios.restlet-ext.jetty.1.1-SNAPSHOT.jar',
+                      'rest-api-oar/lib/jetty-6.1.11.jar',
+                      'rest-api-oar/lib/jetty-ajp-6.1.11.jar',
+                      'rest-api-oar/lib/jetty-sslengine-6.1.11.jar',
+                      'rest-api-oar/lib/jetty-util-6.1.11.jar'
+//                      'rest-api-oar/lib/log4j-1.2.13.jar',
+//                      'rest-api-oar/lib/commons-logging-1.0.4.jar',
+//                      'rest-api-oar/lib/commons-codec-1.2.jar',
+//                      'rest-api-oar/lib/commons-lang-2.3.jar',
+//                      'rest-api-oar/lib/commons-httpclient-3.1.jar',
+//                      'rest-api-oar/lib/servlet-api-2.5.jar',
+//                      'rest-api-oar/lib/jibx-run-1.1.6a.jar',
+//                      'rest-api-oar/lib/jibx-extras-1.1.6a.jar',
+//                      'rest-api-oar/lib/wstx-asl-3.2.1.jar',
+//                      'rest-api-oar/lib/stax-api-1.0-2.jar',
+//                      'rest-api-oar/lib/activation-1.1.jar',
+//                      'rest-api-oar/lib/xalan-2.7.1.jar',
+//                      'rest-api-oar/lib/serializer-2.7.1.jar'
+//                      'rest-api-oar/lib/typica-1.4.1.jar',
+//                      'rest-api-oar/lib/jaxb-impl-2.1.6.jar',
+//                      'rest-api-oar/lib/jaxb-api-2.1.jar'
+//                      'rest-api-oar/lib/spring-2.5.5.jar',    // todo: remove this?
+//                      'rest-api-oar/lib/asm-1.5.3.jar',
+//                      'rest-api-oar/lib/cglib-2.1_3.jar'
+        }
+        serviceLevelAgreements {
+            systemRequirements ref: 'Elastic Grid Platform'
+//            sla(id: 'Queue VideoConversion', low: 2, high: 5) {
+//                policy(type: 'scaling', max: 10, lowerDampener: 3600, upperDampener: 3600)
+//            }
+        }
+        maintain 1
+        maxPerMachine 1
+    }
+}
