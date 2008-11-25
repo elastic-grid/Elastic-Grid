6553c1f11516ff302c7f4785afaa1b77dbf1cbb4
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-11-25 21:24:53 +0000

    Example used for the Wiki.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@168 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- examples/spring-dm-server/springDmServerLocal.groovy
+++ examples/spring-dm-server/springDmServerLocal.groovy
@@ -1,17 +1,13 @@
-deployment(name: 'Spring dm Server') {
+deployment(name: 'Spring DM') {
   groups('rio')
 
-  serviceExec(name: 'Spring dm Server') {
-    software(name: 'Spring dm Server', version: '1.0.0', removeOnDestroy: true) {
-      install source: 'file:///Users/jeje/Documents/Development/eg/deploy/springsource-dm-server-1.0.0.RELEASE.zip',
-              target: '${RIO_HOME}/system/external',
+  serviceExec(name: 'DM Server') {
+    software(name: 'Spring DM', version: '1.0.0', removeOnDestroy: true) {
+      install source: 'file://${EG_HOME}/deploy/springsource-dm-server-1.0.0.RELEASE.zip',
+              target: 'springsource-dm-server',
               unarchive: true
-      postInstall(removeOnCompletion: false) {
-        execute command: '/bin/chmod +x ${RIO_HOME}/system/external/springsource-dm-server-1.0.0.RELEASE/bin/*.sh',
-                nohup: false
-      }
     }
-    execute command: '${RIO_HOME}/system/external/springsource-dm-server-1.0.0.RELEASE/bin/startup.sh'
+    execute command: 'bin/startup.sh'
     maintain 1
   }
 
