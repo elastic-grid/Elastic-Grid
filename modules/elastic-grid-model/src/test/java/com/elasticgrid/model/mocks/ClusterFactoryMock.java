b5f1fb562fd9dbca9258cc64f08a9c362af96609
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-12-05 20:42:23 +0000

    Switch to AGPLv3 license.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@182 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- modules/elastic-grid-model/src/test/java/com/elasticgrid/model/mocks/ClusterFactoryMock.java
+++ modules/elastic-grid-model/src/test/java/com/elasticgrid/model/mocks/ClusterFactoryMock.java
@@ -1,20 +1,19 @@
 /**
+ * Elastic Grid
  * Copyright (C) 2007-2008 Elastic Grid, LLC.
  * 
- * This file is part of Elastic Grid.
- * 
- * Elastic Grid is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
- * License, or any later version.
+ * License, or (at your option) any later version.
  * 
- * Elastic Grid is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Elastic Grid.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.elasticgrid.model.mocks;
