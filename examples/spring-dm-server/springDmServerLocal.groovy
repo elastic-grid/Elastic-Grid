f38184b5ba8d794a429936067ed9462adfdbea1c
Jerome Bernard <jerome.bernard@elastic-grid.com>
2008-12-06 01:24:38 +0000

    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@186 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- examples/spring-dm-server/springDmServerLocal.groovy
+++ examples/spring-dm-server/springDmServerLocal.groovy
@@ -2,13 +2,19 @@ deployment(name: 'Spring DM') {
   groups('rio')
 
   serviceExec(name: 'DM Server') {
-    software(name: 'Spring DM', version: '1.0.0', removeOnDestroy: true) {
-      install source: 'file://${EG_HOME}/deploy/springsource-dm-server-1.0.0.RELEASE.zip',
+    software(name: 'Spring DM', version: '1.0.1', removeOnDestroy: true) {
+      install source: 'file://${RIO_HOME}/deploy/springsource-dm-server-1.0.1.RELEASE.zip',
               target: 'springsource-dm-server',
               unarchive: true
     }
+
+    // deploy Spring Travel Sample
+//    data source: 'file://${EG_HOME}/deploy/spring-travel-1.2.0.zip',
+//         target: 'springsource-dm-server/springsource-dm-server-1.0.1.RELEASE'
+
     execute command: 'bin/startup.sh'
-    maintain 1
+    maintain 1                          // we want at least 1 instance to be running in our cluster
+    maxPerMachine 1                     // we don't want to host more than 1 instance per machine
   }
 
 }
