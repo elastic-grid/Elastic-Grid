f7fb652340460fe463f7bc992aaad82d8064f9bd
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-02-10 23:40:36 +0000

    Service Descriptor for EG's monitor.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@211 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- modules/elastic-grid-core/src/main/java/com/elasticgrid/boot/ServiceDescriptorUtil.java
+++ modules/elastic-grid-core/src/main/java/com/elasticgrid/boot/ServiceDescriptorUtil.java
@@ -26,6 +26,97 @@ import org.rioproject.boot.RioServiceDescriptor;
 
 public class ServiceDescriptorUtil extends org.rioproject.boot.ServiceDescriptorUtil {
 
+    /**
+     * Get the {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * {@link org.rioproject.monitor.ProvisionMonitor} using the Webster port
+     * created by this utility.
+     *
+     * @param policy The security policy file to use
+     * @param monitorConfig The configuration file the Monitor will use
+     * @return The {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * the Monitor using an anonymous port.
+     *
+     * @throws IOException If there are problems getting the anonymous port
+     * @throws RuntimeException If the <tt>EG_HOME</tt> system property is not
+     * set
+     */
+    public static ServiceDescriptor getMonitor(String policy, String monitorConfig) throws IOException {
+        return getMonitor(policy, monitorConfig, null);
+    }
+
+    /**
+     * Get the {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * {@link org.rioproject.monitor.ProvisionMonitor} using the Webster port
+     * created by this utility.
+     *
+     * @param policy The security policy file to use
+     * @param monitorConfig The configuration file the Monitor will use
+     * @param overrides The configuration overrides to use
+     * @return The {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * the Monitor using an anonymous port.
+     *
+     * @throws IOException If there are problems getting the anonymous port
+     * @throws RuntimeException If the <tt>EG_HOME</tt> system property is not
+     * set
+     */
+    public static ServiceDescriptor getMonitor(String policy, String monitorConfig, String[] overrides) throws IOException {
+        return getMonitor(policy, monitorConfig, overrides, getAnonymousPort());
+    }
+
+    /**
+     * Get the {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * {@link org.rioproject.monitor.ProvisionMonitor}.
+     *
+     * @param policy The security policy file to use
+     * @param monitorConfig The configuration file the Monitor will use
+     * @param overrides The configuration overrides to use
+     * @param port The port to use when constructing the codebase
+     * @return The {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * the Monitor using an anonymous port.
+     *
+     * @throws IOException If there are problems getting the anonymous port
+     * @throws RuntimeException If the <tt>EG_HOME</tt> system property is not
+     * set
+     */
+    public static ServiceDescriptor getMonitor(String policy, String monitorConfig, String[] overrides, String port) throws IOException {
+        return getMonitor(policy, monitorConfig, overrides, BootUtil.getHostAddress(), port);
+    }
+
+    /**
+     * Get the {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * {@link org.rioproject.monitor.ProvisionMonitor}.
+     *
+     * @param policy The security policy file to use
+     * @param monitorConfig The configuration file the Monitor will use
+     * @param overrides The configuration overrides to use
+     * @param hostAddress The address to use when constructing the codebase
+     * @param port The port to use when constructing the codebase
+     * @return The {@link com.sun.jini.start.ServiceDescriptor} instance for
+     * the Monitor using an anonymous port.
+     *
+     * @throws IOException If there are problems getting the anonymous port
+     * @throws RuntimeException If the <tt>EG_HOME</tt> system property is not
+     * set
+     */
+    public static ServiceDescriptor getMonitor(String policy, String monitorConfig, String[] overrides, String hostAddress, String port) throws IOException {
+        String egHome = System.getProperty("EG_HOME");
+        if (egHome == null)
+            throw new RuntimeException("EG_HOME property not declared");
+
+        String[] configArgs = getArray(monitorConfig, overrides);
+        String monitorClasspath =
+            egHome + File.separator + "lib" + File.separator + "monitor.jar"
+            + File.pathSeparator +
+            // TODO: get rid of this version number!
+            egHome + File.separator + "lib" + File.separator + "elastic-grid" + File.separator + "kernel" + File.separator + "amazon-ec2-provisioner-0.8.3.jar"
+            ;
+        String monitorCodebase = BootUtil.getCodebase(new String[] { "monitor-dl.jar", "rio-dl.jar", "jsk-dl.jar"}, hostAddress, port);
+        String implClass = "org.rioproject.monitor.ProvisionMonitorImpl";
+        return new RioServiceDescriptor(monitorCodebase, policy, monitorClasspath, implClass, configArgs);
+
+    }
+
+
     public static ServiceDescriptor getRestApi(String policy, String restApiConfig) throws IOException {
         return getRestApi(policy, restApiConfig, null);
     }
@@ -44,7 +135,7 @@ public class ServiceDescriptorUtil extends org.rioproject.boot.ServiceDescriptor
             throw new RuntimeException("EG_HOME property not declared");
         String[] configArgs = getArray(restApiConfig, overrides);
         String restApiRoot = egHome + File.separator + "lib" + File.separator + "elastic-grid" + File.separator + "applications" + File.separator + "rest-api";
-        String restApiClasspath = restApiRoot + File.separator + "rest-api-0.8.3.jar";
+        String restApiClasspath = restApiRoot + File.separator + "rest-api-0.8.3.jar";      // TODO: get rid of this version number!
         String restApiCodebase = BootUtil.getCodebase(new String[] { "rio-dl.jar", "jsk-dl.jar"}, hostAddress, port);
         String implClass = "com.elasticgrid.rest.RestJSB";
         return new RioServiceDescriptor(restApiCodebase, policy, restApiClasspath, implClass, configArgs);
