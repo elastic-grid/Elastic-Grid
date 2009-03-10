aecfdccf7295c64c74b4c94d27fcfce18fce4438
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-03-10 11:52:27 +0000

    Removed deprecated stuff.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@287 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- substrates/mysql/start-mysql.sh
+++
@@ -1,9 +0,0 @@
-groupadd mysql
-useradd -g mysql mysql
-chown -R mysql .
-chgrp -R mysql .
-scripts/mysql_install_db --user=mysql
-chown -R root .
-chown -R mysql data
-#bin/mysqld_safe --user=mysql &
-bin/mysqld --basedir=. --datadir=data --user=mysql --pid-file=mysql.pid --skip-external-locking
