2a751c32a12f3e91e84ab71227f299f604a050c5
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-04-23 13:35:41 +0000

    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@298 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- platforms/private-lan-platform/bin/eg-ui.cmd
+++ platforms/private-lan-platform/bin/eg-ui.cmd
@@ -1,11 +1,31 @@
 @echo off
 
-rem
-rem Launches the Elastic Grid UI
-rem
-
 title Elastic Grid UI
-set EG_HOME="%~dp0.."
+
+rem This script provides the command and control utility for starting
+rem Elastic Grid administration console
+
+rem Use local variables
+setlocal
+
+rem Set local variables
+if "%EG_HOME%" == "" set EG_HOME=%~dp0..
+set RIO_HOME=%EG_HOME%
+if "%JINI_HOME%" == "" goto noJiniHome
+goto haveJiniHome
+
+:noJiniHome
+if not exist "%EG_HOME%\lib\apache-river" goto jiniNotFound
+set JINI_HOME=%EG_HOME%\lib\apache-river
+goto haveJiniHome
+
+:jiniNotFound
+echo Cannot locate expected Jini (River) distribution, either set JINI_HOME or download Elastic Grid with dependencies, exiting
+goto exitWithError
+
+:haveJiniHome
+set JINI_LIB=%JINI_HOME%\lib
+
 set CONFIG=%EG_HOME%\config\ui.groovy
 set command_line=%*
 
