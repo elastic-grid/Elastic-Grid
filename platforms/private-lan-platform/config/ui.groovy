9d1fbe94a57b054df982d525968b59ff6e547add
Dennis Reedy <dennis.reedy@elastic-grid.com>
2009-04-28 13:45:02 +0000

    fixed config
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@305 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- platforms/private-lan-platform/config/ui.groovy
+++ platforms/private-lan-platform/config/ui.groovy
@@ -2,11 +2,11 @@ import org.rioproject.config.Component
 
 @Component('org.rioproject.tools.ui')
 class UI {
-    waitMessage="Waiting to discover the Elastic Grid ...";
-    title="Elastic Grid Management Console";
-    bannerIcon="com/elasticgrid/tools/ui/Elastic-Grid-Logo.png";
-    aboutIcon="com/elasticgrid/tools/ui/Elastic-Grid-Logo.png";
-    aboutInfo="Elastic Grid<br>Management Console<br>";
-    build="Early Access";
-    version="${pom.version}";
+    String waitMessage="Waiting to discover the Elastic Grid ..."
+    String title="Elastic Grid Management Console"
+    String bannerIcon="com/elasticgrid/tools/ui/Elastic-Grid-Logo.png"
+    String aboutIcon="com/elasticgrid/tools/ui/Elastic-Grid-Logo.png"
+    String aboutInfo="Elastic Grid<br>Management Console<br>"
+    String build="Early Access"
+    String version='${pom.version}'
 }
