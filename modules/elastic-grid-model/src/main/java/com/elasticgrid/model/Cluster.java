8e1178edef22af38c64984a180cd9c4132199713
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-05-14 13:04:41 +0000

    Javadoc.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@333 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- modules/elastic-grid-model/src/main/java/com/elasticgrid/model/Cluster.java
+++ modules/elastic-grid-model/src/main/java/com/elasticgrid/model/Cluster.java
@@ -22,20 +22,88 @@ import java.io.Serializable;
 import java.util.Set;
 
 /**
+ * A cluster is a set of {@link Node}s that are linked together
+ * and can be used by one or more monitor in order to deploy
+ * applications.
  * @author Jerome Bernard
  */
 public interface Cluster<N extends Node> extends Serializable {
+    /**
+     * Get the name of the cluster.
+     * @return the name of the cluster
+     */
     String getName();
+
+    /**
+     * Set the name of the cluster.
+     * @param name the name of the cluster
+     * @return the cluster
+     */
     Cluster<N> name(String name);
+
+    /**
+     * Give the running status of the cluster.
+     * TODO: this method should be enhanced so that we can running different statuses like "pending" or so.
+     * @return true if the cluster is running
+     */
     boolean isRunning();
+
+    /**
+     * Return the set of {@link Node}s which are members of this cluster.
+     * @return the nodes
+     */
     Set<N> getNodes();
+
+    /**
+     * Return the set of {@link Node}s which are members of this cluster and
+     * are of {@link NodeProfile#MONITOR} or {@link NodeProfile#MONITOR_AND_AGENT}.
+     * @return the monitors nodes
+     */
     Set<N> getMonitorNodes();
+
+    /**
+     * Return the set of {@link Node}s which are members of this cluster and
+     * are of {@link NodeProfile#AGENT} or {@link NodeProfile#MONITOR_AND_AGENT}.
+     * @return the agents nodes
+     */
     Set<N> getAgentNodes();
+
+    /**
+     * Add nodes into the cluster.
+     * Note: this method does NOT start nodes, it only modifies the list of nodes in the cluster;
+     * it should not be called most of the time.
+     * @param nodes the list of nodes to add to the cluster
+     * @return the cluster
+     */
     Cluster<N> addNodes(Set<N> nodes);
+
+    /**
+     * Return the set of applications deployed within the cluster.
+     * @return the set of applications
+     */
     Set<Application> getApplications();
+
+    /**
+     * Add application into the cluster.
+     * Note: this method does NOT deploy applications, it only modifies the list of applications in the cluster;
+     * it should not be called most of the time.
+     * @param name the name of the new application
+     * @return the application created
+     */
     Application application(String name);
+
+    /**
+     * Add applications into the cluster.
+     * Note: this method does NOT deploy applications, it only modifies the list of applications in the cluster;
+     * it should not be called most of the time.
+     * @param applications the applications
+     * @return the cluster
+     */
     Cluster<N> addApplications(Set<Application> applications);
 
+    /**
+     * Status of the cluster.
+     */
     enum Status {
         RUNNING, STOPPED
     }
