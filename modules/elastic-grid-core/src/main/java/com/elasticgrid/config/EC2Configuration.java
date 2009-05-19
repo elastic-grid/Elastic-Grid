389cd588623a228fdd046d67483143a81ea3e457
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-05-19 14:57:36 +0000

    Refactored the S3 handler so that it can be declared easily from the command-line.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@365 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

---
+++ modules/elastic-grid-core/src/main/java/com/elasticgrid/config/EC2Configuration.java
@@ -0,0 +1,40 @@
+/**
+ * Elastic Grid
+ * Copyright (C) 2008-2009 Elastic Grid, LLC.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.elasticgrid.config;
+
+import com.elasticgrid.config.GenericConfiguration;
+
+/**
+ * Specific EC2 configuration properties.
+ * @author Jerome Bernard
+ */
+public interface EC2Configuration extends GenericConfiguration {
+    public static final String EG_DROP_BUCKET = "eg.s3.dropBucket";
+    public static final String EG_OVERRIDES_BUCKET = "eg.s3.overridesBucket";
+
+    public static final String AWS_ACCESS_ID = "aws.accessId";
+    public static final String AWS_SECRET_KEY = "aws.secretKey";
+    public static final String AWS_EC2_SECURED = "aws.ec2.secured";
+    public static final String AWS_SQS_SECURED = "aws.sqs.secured";
+    public static final String AWS_EC2_KEYPAIR = "aws.ec2.keypair";
+    public static final String AWS_EC2_AMI32 = "aws.ec2.ami32";
+    public static final String AWS_EC2_AMI64 = "aws.ec2.ami64";
+
+    public static final String REDHAT_YUM_PACKAGES = "redhat.yum.packages";
+}
