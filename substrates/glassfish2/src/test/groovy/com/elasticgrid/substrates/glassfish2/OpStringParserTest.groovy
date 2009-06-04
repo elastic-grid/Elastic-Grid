7b8c3256448504f2200d2da85bad276e640c0417
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-06-04 13:35:19 +0000

    Fixed license headers.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@445 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- substrates/glassfish2/src/test/groovy/com/elasticgrid/substrates/glassfish2/OpStringParserTest.groovy
+++ substrates/glassfish2/src/test/groovy/com/elasticgrid/substrates/glassfish2/OpStringParserTest.groovy
@@ -1,19 +1,20 @@
 /**
- * Copyright (C) 2007-2008 Elastic Grid, LLC.
- * 
- * Licensed under the GNU Lesser General Public License, Version 3.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         http://www.gnu.org/licenses/lgpl-3.0.html
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Elastic Grid
+ * Copyright (C) 2008-2009 Elastic Grid, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.elasticgrid.substrates.glassfish2
 
 import org.rioproject.core.provision.StagedSoftware
