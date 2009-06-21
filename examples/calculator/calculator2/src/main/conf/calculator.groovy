10a3571005540809ebf81ffa933b97e7965a4f06
Jérôme Bernard <jeje@ip80-101-237-154.hotspotsvankpn.com>
2009-06-21 12:03:46 +0200

    Fixed calculator examples.

--- examples/calculator/calculator2/src/main/conf/calculator.groovy
+++ examples/calculator/calculator2/src/main/conf/calculator.groovy
@@ -1,5 +1,5 @@
 deployment(name:'Calculator 2') {
-    groups 'calculator'
+    groups 'elastic-grid'
 
     resources id:'impl.jars', 'calculator2_0.9.1/lib/calculator2-0.9.1-impl.jar'
     resources id:'client.jars', 'calculator2_0.9.1/lib/calculator2-0.9.1-dl.jar'
@@ -18,6 +18,10 @@ deployment(name:'Calculator 2') {
             association name:'Multiply', type:'uses', property:'multiply'
             association name:'Divide', type:'uses', property:'divide'
             association name:'Modulo', type:'uses', property:'modulo'
+        }
+        sla(id: 'time', high: 400000) {
+          policy type: 'scaling', max: 3, lowerDampener: 3000, upperDampener: 3000
+          monitor name: 'time', property: 'time', period: 5000
         }
         maintain 1
     }
