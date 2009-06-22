170e23b23c76f908a152507cd81e4b970a9a0965
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-06-22 19:14:35 +0200

    Initial support for Mosso Cloud Files.

---
+++ modules/mosso-cloudfiles-storage/src/main/java/com/elasticgrid/storage/mosso/cloudfiles/MossoStorageManager.java
@@ -0,0 +1,68 @@
+/**
+ * Elastic Grid
+ * Copyright (C) 2008-2009 Elastic Grid, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.elasticgrid.storage.mosso.cloudfiles;
+
+import com.elasticgrid.storage.Container;
+import com.elasticgrid.storage.StorageException;
+import com.elasticgrid.storage.StorageManager;
+import com.mosso.client.cloudfiles.FilesClient;
+import com.mosso.client.cloudfiles.FilesContainer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link StorageManager} providing support for Mosso Cloud Files.
+ *
+ * @author Jerome Bernard
+ */
+public class MossoStorageManager implements StorageManager {
+    private FilesClient mosso;
+
+    public MossoStorageManager(String login, String password) {
+        mosso = new FilesClient(login, password);
+    }
+
+    public List<Container> getContainers() throws StorageException {
+        try {
+            mosso.login();
+            List<FilesContainer> mossoContainers = mosso.listContainers();
+            List<Container> containers = new ArrayList<Container>(mossoContainers.size());
+            for (FilesContainer mossoContainer : mossoContainers) {
+                containers.add(new MossoContainer(mosso, mossoContainer));
+            }
+            return containers;
+        } catch (Exception e) {
+            throw new StorageException("Can't get list of containers", e);
+        }
+    }
+
+    public Container createContainer(String name) throws StorageException {
+        try {
+            mosso.login();
+            mosso.createContainer(name);
+            FilesContainer mossoContainer = null;
+            List<FilesContainer> containers = mosso.listContainers();
+            for (FilesContainer container : containers)
+                if (container.getName().equals(name))
+                    mossoContainer = container;
+            return new MossoContainer(mosso, mossoContainer);
+        } catch (Exception e) {
+            throw new StorageException("Can't create container", e);
+        }
+    }
+}
