7efc53ec7c260f6b85349b71eec3f8587c83d440
dennisreedy <dennisreedy@e25e3cd6-ff2c-412d-a5cc-22119c6ca403>
2009-08-05 22:11:49 +0000

    Add ClientSocketFactory to use public hostname for the OpStringManager exporter
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@502 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- platforms/private-lan/config/monitor.groovy
+++ platforms/private-lan/config/monitor.groovy
@@ -7,6 +7,7 @@ import com.elasticgrid.platforms.ec2.discovery.EC2LookupDiscoveryManager
 import org.rioproject.core.ClassBundle
 import org.rioproject.config.Component
 import org.rioproject.config.Constants
+import org.rioproject.config.ClientSocketFactory
 import org.rioproject.fdh.FaultDetectionHandlerFactory
 import org.rioproject.monitor.DeployHandler
 import org.rioproject.monitor.FileSystemOARDeployHandler
@@ -27,81 +28,86 @@ import java.util.logging.Level
  */
 @Component ('org.rioproject.monitor')
 class MonitorConfig {
-  String serviceName = 'Elastic Grid Monitor'
-  String serviceComment = 'Elastic Grid Dynamic Provisioning Agent'
-  long deployMonitorPeriod = 30000
+    String serviceName = 'Elastic Grid Monitor'
+    String serviceComment = 'Elastic Grid Dynamic Provisioning Agent'
+    long deployMonitorPeriod = 30000
 
-  String[] getInitialOpStrings() {
-    def opstrings = []
-    File egLibsDirectory = new File(System.getProperty('EG_HOME') + File.separatorChar + 'lib' + File.separatorChar + 'elastic-grid');
-    def egConfigDirectory = System.getProperty('EG_HOME') + '/config'
-    opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-core-services.groovy'
-    egLibsDirectory.eachFileMatch(~".*platform.*-impl.jar") {File f ->
-      if (f.name.startsWith('private-lan-cloud-platform'))
-        opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-private-lan-services.groovy'
-      if (f.name.startsWith('amazon-ec2-cloud-platform'))
-        opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-amazon-services.groovy'
-      else if (f.name.startsWith('rackspace-cloud-platform'))
-        opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-rackspace-services.groovy'
+    String[] getInitialOpStrings() {
+        def opstrings = []
+        File egLibsDirectory = new File(System.getProperty('EG_HOME') + File.separatorChar + 'lib' + File.separatorChar + 'elastic-grid');
+        def egConfigDirectory = System.getProperty('EG_HOME') + '/config'
+        opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-core-services.groovy'
+        egLibsDirectory.eachFileMatch(~".*platform.*-impl.jar") {File f ->
+            if (f.name.startsWith('private-lan-cloud-platform'))
+                opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-private-lan-services.groovy'
+            if (f.name.startsWith('amazon-ec2-cloud-platform'))
+                opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-amazon-services.groovy'
+            else if (f.name.startsWith('rackspace-cloud-platform'))
+                opstrings << egConfigDirectory + File.separatorChar + 'elastic-grid-rackspace-services.groovy'
+        }
+        return opstrings as String[]
     }
-    return opstrings as String[]
-  }
 
-  String[] getInitialLookupGroups() {
-    if (System.getProperty(Constants.GROUPS_PROPERTY_NAME) == null)
-      System.setProperty(Constants.GROUPS_PROPERTY_NAME, 'elastic-grid')
-    def groups = [System.getProperty(Constants.GROUPS_PROPERTY_NAME)]
-    return groups as String[]
-  }
-
-  LookupLocator[] getInitialLookupLocators() {
-    String locators = System.getProperty(Constants.LOCATOR_PROPERTY_NAME)
-    if (locators != null) {
-      def lookupLocators = JiniClient.parseLocators(locators)
-      return lookupLocators as LookupLocator[]
-    } else {
-      return null
+    String[] getInitialLookupGroups() {
+        if (System.getProperty(Constants.GROUPS_PROPERTY_NAME) == null)
+            System.setProperty(Constants.GROUPS_PROPERTY_NAME, 'elastic-grid')
+        def groups = [System.getProperty(Constants.GROUPS_PROPERTY_NAME)]
+        return groups as String[]
     }
-  }
 
-  ServiceResourceSelector getServiceResourceSelector() {
-    return new LeastActiveSelector()
-  }
+    LookupLocator[] getInitialLookupLocators() {
+        String locators = System.getProperty(Constants.LOCATOR_PROPERTY_NAME)
+        if (locators != null) {
+            def lookupLocators = JiniClient.parseLocators(locators)
+            return lookupLocators as LookupLocator[]
+        } else {
+            return null
+        }
+    }
 
-  /**
-   * Use a JrmpExporter for the OpStringManager.
-   */
-  Exporter getOpStringManagerExporter() {
-    return new JrmpExporter()
-  }
+    ServiceResourceSelector getServiceResourceSelector() {
+        return new LeastActiveSelector()
+    }
 
-  LoggerConfig[] getLoggerConfigs() {
-    def loggers = []
-    [
+    /**
+     * Use a JrmpExporter for the OpStringManager.
+     */
+    Exporter getOpStringManagerExporter() {
+        Exporter getOpStringManagerExporter() {
+            File pubHostName = new File("/tmp/public-hostname")
+            return new JrmpExporter(0,
+                                    new ClientSocketFactory(pubHostName.text),
+                                    null)
+        }
+    }
+
+    LoggerConfig[] getLoggerConfigs() {
+        def loggers = []
+        [
             'org.rioproject.monitor': Level.INFO,
             'net.jini.lookup.JoinManager': Level.OFF,
             'org.apache.commons.httpclient': Level.WARNING,
-    ].each {name, level ->
-      loggers << new LoggerConfig(name, level, new LogHandlerConfig(new ConsoleHandler()))
+        ].each {name, level ->
+            loggers << new LoggerConfig(name, level, new LogHandlerConfig(new ConsoleHandler()))
+        }
+        return loggers as LoggerConfig[]
     }
-    return loggers as LoggerConfig[]
-  }
 
-  ClassBundle getFaultDetectionHandler() {
-    def fdh = org.rioproject.fdh.HeartbeatFaultDetectionHandler.class.name
-    def fdhConf = ['-', fdh + '.heartbeatPeriod=10000', fdh + 'heartbeatGracePeriod=10000']
-    return FaultDetectionHandlerFactory.getClassBundle(fdh, fdhConf)
-  }
+    ClassBundle getFaultDetectionHandler() {
+        def fdh = org.rioproject.fdh.HeartbeatFaultDetectionHandler.class.name
+        def fdhConf = ['-', fdh + '.heartbeatPeriod=10000', fdh + 'heartbeatGracePeriod=10000']
+        return FaultDetectionHandlerFactory.getClassBundle(fdh, fdhConf)
+    }
 
-  /* Configure DeployHandlers for the Monitor to use */
+    /* Configure DeployHandlers for the Monitor to use */
 
-  DeployHandler[] getDeployHandlers() {
-    def deployDir = System.getProperty('RIO_HOME') + '/deploy'
-    return [
+    DeployHandler[] getDeployHandlers() {
+        def deployDir = System.getProperty('RIO_HOME') + '/deploy'
+        return [
             new FileSystemOARDeployHandler(new File(deployDir)),
             new com.elasticgrid.monitor.S3OARDeployHandler(AWSUtils.getDropBucket(), new File(deployDir))
-    ] as DeployHandler[]
-  }
+        ] as DeployHandler[]
+    }
 
 }
 
@@ -110,5 +116,5 @@ class MonitorConfig {
  */
 @Component ('org.rioproject.resources.client.DiscoveryManagementPool')
 class MonitorSharedDiscoveryManagerConfig {
-  String sharedDiscoveryManager = EC2LookupDiscoveryManager.class.getName()
+    String sharedDiscoveryManager = EC2LookupDiscoveryManager.class.getName()
 }
