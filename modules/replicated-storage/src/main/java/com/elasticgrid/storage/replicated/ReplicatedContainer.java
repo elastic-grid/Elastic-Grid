6f6751df08f650f4170e4131ad74702d9ddf1151
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-08-11 17:10:27 +0200

    Added dumb replicated storage engine.

---
+++ modules/replicated-storage/src/main/java/com/elasticgrid/storage/replicated/ReplicatedContainer.java
@@ -0,0 +1,73 @@
+/**
+ * Elastic Grid
+ * Copyright (C) 2008-2009 Elastic Grid, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.elasticgrid.storage.replicated;
+
+import com.elasticgrid.storage.Container;
+import com.elasticgrid.storage.Storable;
+import com.elasticgrid.storage.StorageException;
+import com.elasticgrid.storage.StorableNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * Replicated {@link Container} ensuring that many {@link Container}s are kept in sync each time
+ * a write operation is done. All read operations are done against a preferred {@link Container}
+ *
+ * @author Jerome Bernard
+ */
+public class ReplicatedContainer implements Container {
+    private final Container preferred;
+    private final List<Container> otherContainers;
+    private static final Logger logger = Logger.getLogger(ReplicatedContainer.class.getName());
+
+    public ReplicatedContainer(Container preferred, Container... otherContainers) {
+        this.preferred = preferred;
+        this.otherContainers = Arrays.asList(otherContainers);
+    }
+
+    public String getName() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public List<Storable> listStorables() throws StorageException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Storable findStorableByName(String name) throws StorableNotFoundException, StorageException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Storable uploadStorable(File file) throws StorageException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Storable uploadStorable(String name, File file) throws StorageException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Storable uploadStorable(String name, InputStream stream, String mimeType) throws StorageException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void deleteStorable(String name) throws StorageException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ modules/replicated-storage/src/test/java/com/elasticgrid/storage/replicated/ReplicatedStorageEngineWithFailingPreferredStorageEngineTest.java
@@ -0,0 +1,116 @@
+/**
+ * Elastic Grid
+ * Copyright (C) 2008-2009 Elastic Grid, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.elasticgrid.storage.replicated;
+
+import com.elasticgrid.config.EC2Configuration;
+import com.elasticgrid.config.RackspaceConfiguration;
+import com.elasticgrid.storage.StorageException;
+import com.elasticgrid.storage.ContainerNotFoundException;
+import com.elasticgrid.storage.Container;
+import com.elasticgrid.storage.rackspace.CloudFilesStorageEngine;
+import com.elasticgrid.storage.amazon.s3.S3StorageEngine;
+import com.elasticgrid.storage.spi.AbstractStorageEngineTest;
+import com.elasticgrid.storage.spi.StorageEngine;
+import com.elasticgrid.utils.amazon.AWSUtils;
+import com.elasticgrid.utils.rackspace.RackspaceUtils;
+import java.util.Properties;
+import java.util.List;
+import java.rmi.RemoteException;
+import static org.mockito.Mockito.*;
+import org.testng.annotations.Test;
+import org.apache.commons.lang.math.RandomUtils;
+
+/**
+ * {@link ReplicatedStorageEngine} tests.
+ *
+ * @author Jerome Bernard
+ */
+public class ReplicatedStorageEngineWithFailingPreferredStorageEngineTest {
+
+    @Test(expectedExceptions = StorageException.class)
+    public void testCreationOfContainers() throws StorageException, RemoteException {
+        StorageEngine mgr = getStorageEngine();
+        // create a container
+        String containerName = getTestContainer();
+        Container container = mgr.createContainer(containerName);
+    }
+
+    @Test(expectedExceptions = {ContainerNotFoundException.class})
+    public void testFindUnknownContainer() throws StorageException, RemoteException {
+        StorageEngine mgr = getStorageEngine();
+        mgr.findContainerByName("whatever-" + RandomUtils.nextInt());
+    }
+
+    @Test(expectedExceptions = {StorageException.class})
+    public void testDeletionOfUnknownContainer() throws StorageException, RemoteException {
+        StorageEngine mgr = getStorageEngine();
+        mgr.deleteContainer("whatever-" + RandomUtils.nextInt());
+    }
+
+    protected String getTestContainer() {
+        return "test-" + RandomUtils.nextInt();
+    }
+
+    protected StorageEngine getStorageEngine() throws StorageException, RemoteException {
+        return new ReplicatedStorageEngine(
+                createAmazonStorageEngine(),
+                createRackspaceStorageEngine()
+        );
+    }
+
+    private StorageEngine createRackspaceStorageEngine() throws StorageException {
+        try {
+            Properties rackspaceConfig = RackspaceUtils.loadRackspaceConfiguration();
+            String login = rackspaceConfig.getProperty(RackspaceConfiguration.LOGIN);
+            String password = rackspaceConfig.getProperty(RackspaceConfiguration.API_KEY);
+            if (login == null) {
+                throw new IllegalArgumentException("Could not find Cloud Files login");
+            }
+            if (password == null) {
+                throw new IllegalArgumentException("Could not find Cloud Files password");
+            }
+            return new CloudFilesStorageEngine(login, password);
+        } catch (Exception e) {
+            throw new StorageException("Can't initialize Cloud Files Storage Manager", e);
+        }
+    }
+
+    private StorageEngine createAmazonStorageEngine() throws StorageException, RemoteException {
+        StorageEngine mock = mock(StorageEngine.class);
+        when(mock.getStorageName()).thenReturn("Mocked Storage Engine");
+        StorageException storageException = new StorageException("Simulating storage engine failure");
+        doThrow(storageException).when(mock).deleteContainer(anyString());
+        doThrow(storageException).when(mock).findContainerByName(anyString());
+        doThrow(storageException).when(mock).createContainer(anyString());
+        return mock;
+//        try {
+//            Properties awsConfig = AWSUtils.loadEC2Configuration();
+//            String awsAccessID = awsConfig.getProperty(EC2Configuration.AWS_ACCESS_ID);
+//            String awsSecretKey = awsConfig.getProperty(EC2Configuration.AWS_SECRET_KEY);
+//            if (awsAccessID == null) {
+//                throw new IllegalArgumentException("Could not find AWS Access ID");
+//            }
+//            if (awsSecretKey == null) {
+//                throw new IllegalArgumentException("Could not find AWS Secret Key");
+//            }
+//            return new S3StorageEngine(awsAccessID, awsSecretKey);
+//        } catch (Exception e) {
+//            throw new StorageException("Can't initialize S3 Storage Manager", e);
+//        }
+    }
+}
