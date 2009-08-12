c3b43924404885f5da5c309dfa137136f7493677
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-08-12 15:02:32 +0200

    Register JetS3t MBeans automatically.

--- platforms/private-lan/bin/setup-env.cmd
+++ platforms/private-lan/bin/setup-env.cmd
@@ -1,8 +1,1 @@
-if [ -f /tmp/user-data ] ; then
-    source /tmp/user-data
-fi
-if [ -f /tmp/public-hostname ] ; then
-    export NET_ADDR=`cat /tmp/public-hostname`
-fi
-
-export EG_OPTS="-Dorg.rioproject.groups=$CLUSTER_NAME -Dcom.sun.management.jmxremote"
+set EG_OPTS=-Dcom.sun.management.jmxremote
