7194b5d3e40d2873031d3453a02568cb04c5a236
Jerome Bernard <jerome.bernard@elastic-grid.com>
2009-11-16 19:29:46 +0100

    Fixes in the assembly.

--- platforms/private-lan/bin/eg-ui.cmd
+++ platforms/private-lan/bin/eg-ui.cmd
@@ -11,23 +11,9 @@ setlocal
 rem Set local variables
 if "%EG_HOME%" == "" set EG_HOME=%~dp0..
 set RIO_HOME=%EG_HOME%
-if "%JINI_HOME%" == "" goto noJiniHome
-goto haveJiniHome
-
-:noJiniHome
-if not exist "%EG_HOME%\lib\apache-river" goto jiniNotFound
-set JINI_HOME=%EG_HOME%\lib\apache-river
-goto haveJiniHome
-
-:jiniNotFound
-echo Cannot locate expected Jini (River) distribution, either set JINI_HOME or download Elastic Grid with dependencies, exiting
-goto exitWithError
-
-:haveJiniHome
-set JINI_LIB=%JINI_HOME%\lib
 
 set CONFIG=%EG_HOME%\config\ui.groovy
 set command_line=%*
 
-"%JAVA_HOME%\bin\java" -jar "%~dp0../lib/rio-ui.jar" %CONFIG% %command_line%
+"%JAVA_HOME%\bin\java" -jar "%~dp0../lib/rio-ui.jar" %command_line%
 
