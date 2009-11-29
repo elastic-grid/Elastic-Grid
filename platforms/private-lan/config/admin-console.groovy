0e2ad88e36ac3812b4be28c1e9e4a7983cac9bf3
dennisreedy <dennisreedy@e25e3cd6-ff2c-412d-a5cc-22119c6ca403>
2009-11-29 14:52:52 +0000

    banner
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/elastic-grid/trunk@518 e25e3cd6-ff2c-412d-a5cc-22119c6ca403

--- platforms/private-lan/config/admin-console.groovy
+++ platforms/private-lan/config/admin-console.groovy
@@ -1,16 +1,16 @@
 import org.rioproject.config.Component
 import com.elasticgrid.boot.BannerProviderImpl
-import org.rioproject.resources.util.BannerProvider
+import org.rioproject.util.BannerProvider
 
 /**
  * Configures the web administration console
  */
-@Component ('com.elasticgrid.admin')
+@Component('com.elasticgrid.admin')
 class AdminConsoleConfig {
-  int port = 8080
-  String context = '/'
+    int port = 8080
+    String context = '/'
 
-  BannerProvider getBannerProvider() {
-    return new BannerProviderImpl()
-  }
+    BannerProvider getBannerProvider() {
+        return new BannerProviderImpl()
+    }
 }
--- platforms/private-lan/config/rest-api.groovy
+++ platforms/private-lan/config/rest-api.groovy
@@ -8,36 +8,30 @@ import java.util.logging.Level
 import org.rioproject.log.LoggerConfig
 import org.rioproject.log.LoggerConfig.LogHandlerConfig
 import java.util.logging.ConsoleHandler
-import com.elasticgrid.boot.BannerProviderImpl
-import org.rioproject.resources.util.BannerProvider
 
 /*
  * Declare REST API properties
  */
-@Component ('com.elasticgrid.rest')
+@Component('com.elasticgrid.rest')
 class RestServiceConfig {
-  String serviceName = 'Elastic Grid REST API'
-  String serviceComment = 'Elastic Grid REST API'
-  String jmxName = 'com.elasticgrid.api:type=REST'
+    String serviceName = 'Elastic Grid REST API'
+    String serviceComment = 'Elastic Grid REST API'
+    String jmxName = 'com.elasticgrid.api:type=REST'
 
-  String[] getInitialLookupGroups() {
-    def groups = [System.getProperty(Constants.GROUPS_PROPERTY_NAME, 'elastic-grid')]
-    return groups as String[]
-  }
+    String[] getInitialLookupGroups() {
+        def groups = [System.getProperty(Constants.GROUPS_PROPERTY_NAME, 'elastic-grid')]
+        return groups as String[]
+    }
 
-  LoggerConfig[] getLoggerConfigs() {
-    def loggers = []
-    [
+    LoggerConfig[] getLoggerConfigs() {
+        def loggers = []
+        [
             'org.reslet': Level.FINEST,
             'com.noelios': Level.FINEST,
-    ].each {name, level ->
-      loggers << new LoggerConfig(name, level, new LogHandlerConfig(new ConsoleHandler()))
+        ].each {name, level ->
+            loggers << new LoggerConfig(name, level, new LogHandlerConfig(new ConsoleHandler()))
+        }
+        return loggers as LoggerConfig[]
     }
-    return loggers as LoggerConfig[]
-  }
-
-  BannerProvider getBannerProvider() {
-    return new BannerProviderImpl()
-  }
 
 }
